15453451732f9f9a063dec8c194d1a27620fdc02
Rob Bell <rob.bell@asos.com>
2017-12-19 17:15:15 +0000

    Add empty project and beginnings of day 1

---
+++ Day1/RobotCaptcha.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace Day1
+{
+    public class RobotCaptcha
+    {
+        public int Sum(string input)
+        {
+            return 0;
+        }
+    }
+
+    public class RobotCaptchaShould
+    {
+        [Fact]
+        public void SumIdenticalDigitsWhichAreContiguous()
+        {
+            const string input = "1224";
+
+            var captcha = new RobotCaptcha();
+
+            var total = captcha.Sum(input);
+
+            Assert.Equal(4, total);
+        }
+    }
+}
